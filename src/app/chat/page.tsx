e0156119ccbb1c23f9a34a9c0768f35bfee38fc5
MikeLautensack <mikelautensack100@gmail.com>
2024-08-23 15:56:38 -0400

    Updated chat page heading styles

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -9,7 +9,9 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Typography>Chat Page</Typography>
+      <Typography variant="h4" color="primary">
+        Chat Page
+      </Typography>
       <Chat />
     </main>
   );
