507d258309f62099be55e5f8bac259df1a4d1f80
MikeLautensack <mikelautensack100@gmail.com>
2024-08-24 12:02:10 -0400

    fixed text overlap on mobile size screens

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <main className="flex h-screen flex-col items-center justify-between p-4 gap-4 relative">
-      <div className="w-full flex justify-center items-center h-11">
+      <div className="w-full flex justify-start items-center h-11">
         <Typography variant="h4" color="primary">
           Chat Page
         </Typography>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Typography } from "@mui/material";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-8">
-      <Typography variant="h3" color="primary">
+      <Typography variant="h3" color="primary" textAlign="center">
         Sign In to Enter Chat
       </Typography>
       <Login />
