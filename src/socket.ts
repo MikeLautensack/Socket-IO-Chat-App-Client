65dc7aa7de8868edffe3869dbdd911839b93cc49
MikeLautensack <mikelautensack100@gmail.com>
2024-09-13 20:34:46 -0400

    update url

--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,10 @@
 import { io, Socket } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-// const URL =
-//   "https://socket-io-chat-app-server-daewhfc4f9aad7et.eastus-01.azurewebsites.net/";
+const URL =
+  "https://socket-io-chat-app-server-daewhfc4f9aad7et.eastus-01.azurewebsites.net/";
 
-const URL = "http://localhost:8080";
+// const URL = "http://localhost:8080";
 
 // export const socket = io(URL!, {
 //   autoConnect: false,
